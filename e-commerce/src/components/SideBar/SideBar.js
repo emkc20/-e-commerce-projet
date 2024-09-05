02b7c07c0b35b8cda83748ccc743a02c2e79a87b
Mustafa Ilhan Emekci <emkc20@gmail.com>
2024-09-05 17:00:26 +0300

    filtreleme islemleri yapildi

--- e-commerce/src/components/SideBar/SideBar.js
+++ e-commerce/src/components/SideBar/SideBar.js
@@ -1,21 +1,89 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCategories } from '../../redux/CategoriesSlice';
+import './SideBar.css';
+import { productsCategoryFilter, setPriceRange, setRatingStar, setSortBy } from '../../redux/ProductsSlice';
+import StarRatings from 'react-star-ratings';
 
-function SideBar(props) {
+function SideBar() {
+  const priceRange = [
+    { min: 0, max: 100 }, { min: 100, max: 250 }, { min: 250, max: 1000 },
+  ];
   const dispatch = useDispatch();
   const { categories } = useSelector((state) => state.categories);
 
-
   useEffect(() => {
-      dispatch(getCategories());
+      fetchCategories();
     }, [dispatch],
   );
+
+  const fetchCategories = () => {
+    dispatch(getCategories());
+  };
+
+  const handleClick = (category) => {
+    dispatch(productsCategoryFilter(category));
+  };
+
+  const filteredPrice = (item) => {
+    dispatch(setPriceRange(item));
+  };
+  const filteredStar = () => {
+    dispatch(setRatingStar());
+  };
+  const handleSortChange = (short) => {
+    dispatch(setSortBy(short));
+  };
+
   return (
-    <div>
-      SideBar
+    <div className="side-bar">
+      <div className="side-bar-title">Kategoriler</div>
+      {categories.length > 0 &&
+        <ul className="side-bar-list">
+          <li className="side-bar-item" onClick={() => fetchCategories()}>Tümü</li>
+          {categories.map((category, index) =>
+            <li key={index} onClick={() => handleClick(category)}
+                className="side-bar-item">{category}
+            </li>)}
+        </ul>
+      }
+
+      <div className="side-bar-price-range side-bar-section">
+        <p>Fiyat Aralığına Göre Sırala</p>
+        <div className="side-bar-price">
+          {priceRange.map(price =>
+            <p className="price-item" onClick={() => filteredPrice(price)}>{price.min} - {price.max} </p>)}
+        </div>
+      </div>
+
+      <div className="side-bar-section side-bar-star">
+        <p className="mb-2">Ürün Puanı</p>
+        <div onClick={filteredStar} className="side-bar-star-container">
+          <StarRatings
+            rating={4.5}
+            starRatedColor="#FFD700"
+            numberOfStars={5}
+            name="rating"
+            starDimension="12px"
+            starSpacing="3px"
+          />
+          <span>4.5 Yıldız ve Üzeri</span>
+        </div>
+      </div>
+
+      <div className="side-bar-section side-bar-select">
+        <p className="mb-2">Sırala</p>
+        <div className="side-bar-star-container">
+          <select className="side-bar-price-select" onChange={(e) => handleSortChange(e.target.value)} name="" id="">
+            <option disabled value="">Seçiniz</option>
+            <option value="inc">Artan</option>
+            <option value="dec">Azalan</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
+
 }
 
 export default SideBar;
--- e-commerce/src/redux/ProductsSlice.js
+++ e-commerce/src/redux/ProductsSlice.js
@@ -5,9 +5,11 @@ import axios from 'axios';
 
 const initialState = {
   products: [],
+  initialProducts: [],
   productsStatus: STATUS.IDLE,
   productsDetail: [],
   productsDetailStatus: STATUS.IDLE,
+  sortBy: 'inc',
 };
 
 export const getproducts = createAsyncThunk('getProducts', async () => {
@@ -15,10 +17,41 @@ export const getproducts = createAsyncThunk('getProducts', async () => {
   return response.data;
 });
 
+export const productsCategoryFilter = createAsyncThunk('productsCategoryFilter', async (category) => {
+  const response = await axios.get(`https://fakestoreapi.com/products/category/${category}`);
+  return response.data;
+});
+
 const productsSlice = createSlice({
   name: 'products',
   initialState,
-  reducers: {},
+  reducers: {
+    setPriceRange: (state, action) => {
+      state.products = state.initialProducts;
+      const priceStort = state.products.filter(item => item.price >= action.payload.min && item.price <= action.payload.max);
+      state.products = priceStort;
+    },
+    setRatingStar: (state, action) => {
+      state.products = state.initialProducts;
+      const priceStort = state.products.filter(item => item.rating.rate >= 4.5);
+      state.products = priceStort;
+    },
+    setSortBy: (state, action) => {
+      console.log('sss', action);
+      state.products = state.initialProducts;
+      state.sortBy = action.payload;
+      const priceStort = state.products.slice().sort((a, b) => {
+        if (state.sortBy === 'inc') {
+          return a.price - b.price;
+        } else {
+          return b.price - a.price;
+        }
+      });
+
+      state.products = priceStort;
+
+    },
+  },
   extraReducers: (builder) => {
 
     builder.addCase(getproducts.pending, (state, action) => {
@@ -27,10 +60,22 @@ const productsSlice = createSlice({
 
     builder.addCase(getproducts.fulfilled, (state, action) => {
       state.products = action.payload;
+      state.initialProducts = action.payload;
       state.productsStatus = STATUS.SUCCESS;
+      console.log('state.initialProducts', state.initialProducts);
+    });
 
+    builder.addCase(productsCategoryFilter.pending, (state, action) => {
+      state.productsStatus = STATUS.LOADING;
+    });
+
+    builder.addCase(productsCategoryFilter.fulfilled, (state, action) => {
+      state.initialProducts = action.payload;
+      state.products = action.payload;
+      state.productsStatus = STATUS.SUCCESS;
     });
   },
 });
 
+export const { setPriceRange, setRatingStar, setSortBy } = productsSlice.actions;
 export default productsSlice.reducer;
